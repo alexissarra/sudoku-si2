254838366a557f03efbaf1b75eecdb0bcf50510b
Ael <alex.boulon7@gmail.com>
2023-12-14 17:54:59 +0100

    algo

--- main/algorithm/Untitled-2.py
+++ main/algorithm/Untitled-2.py
@@ -1,34 +1,46 @@
 
 
-grille_des_chiffres = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+grille_des_chiffres = [[1], [2], [3], [4], [5], [6], [7], [8]]
 
-region = [0, 0, 1, 0, 0, 6, 0, 8, 0]
-def test(region):
-    grille_temporaire = []
-    for number in region: # pour un élément dans une région
-            if number == 0:
-                for element in grille_des_chiffres : # je parcours la grille des chiffres
-                    if element not in region:      
-                                try:
-                                    boolean = region[region.index(number) - 1] != element
-                                    try:
-                                        boolean = region[region.index(number) + 1] != element
-                                        if boolean is True : grille_temporaire.append(element)
-                                    except IndexError:
-                                        grille_temporaire.append(element)
-                                except IndexError:
-                                    try:
-                                        boolean = region[region.index(number) + 1] != element
-                                        if boolean is True : grille_temporaire.append(element)
-                                    except:
-                                        pass
-                break
-    return grille_temporaire
+for region in grille_des_chiffres:
+    if grille_des_chiffres.index(region) % 3 == 0:
+        print(0)
+    elif grille_des_chiffres.index(region) % 3 == 1:
+        print(1)
+    else:
+        print(2)
+   
+
+
+
+
+# region = [0, 0, 1, 0, 0, 6, 0, 8, 0]
+# def test(region):
+#     grille_temporaire = []
+#     for number in region: # pour un élément dans une région
+#             if number == 0:
+#                 for element in grille_des_chiffres : # je parcours la grille des chiffres
+#                     if element not in region:      
+#                                 try:
+#                                     boolean = region[region.index(number) - 1] != element
+#                                     try:
+#                                         boolean = region[region.index(number) + 1] != element
+#                                         if boolean is True : grille_temporaire.append(element)
+#                                     except IndexError:
+#                                         grille_temporaire.append(element)
+#                                 except IndexError:
+#                                     try:
+#                                         boolean = region[region.index(number) + 1] != element
+#                                         if boolean is True : grille_temporaire.append(element)
+#                                     except:
+#                                         pass
+#                 break
+#     return grille_temporaire
 
 
 
                 
 
 
-print(test(region))
-print(grille_des_chiffres.index(2))
+# print(test(region))
+# print(grille_des_chiffres.index(2))
--- main/algorithm/test.py
+++ main/algorithm/test.py
@@ -1,6 +1,9 @@
 # Création d'une matrice valide connue via internet
 import copy
 import itertools
+import numpy as np
+
+
 def chercher_les_possibilites_ligne_colonne(region : list) -> list :
     print(region)
     
@@ -37,10 +40,6 @@ def possibilite_recu(possibilite):
     
     return possibilite
 
-
-
-import numpy as np
-
 matrice_valide = np.array([
                             [5, 9, 7, 2, 1, 8, 6, 3, 4], [4, 3, 1, 5, 7, 6, 2, 8, 9], [6, 2, 8, 3, 9, 4, 1, 7, 5],
                             [4, 8, 6, 1, 2, 5, 9, 7, 3], [7, 1, 3, 6, 9, 4, 8, 5, 2], [9, 5, 2, 7, 8, 3, 4, 1, 6],
@@ -54,6 +53,7 @@ matrice_a_trou = np.array([
                             [3, 5, 0, 0, 4, 2, 0, 0, 0], [0, 4, 0, 1, 0, 0, 3, 0, 0], [2, 0, 7, 8, 0, 9, 0, 0, 1]
                            ])
 
+print(matrice_a_trou)
 # Essayer d'évaluer les possibilités par région (carré de 3x3)
 
 possibilite_totales = []
@@ -68,23 +68,140 @@ for region in matrice_a_trou: # je détermine pour chaque région les possibilit
     possibilite_totales.append(list(itertools.permutations(possibilites_ligne_region))) 
     
     compteur += len(list(itertools.permutations(possibilites_ligne_region)) ) # il y a n! permutations possibles par matrice
-print(possibilite_totales, len(possibilite_totales), compteur)
+# print(possibilite_totales, len(possibilite_totales), compteur)
 
-print(possibilite_totales[1], len(possibilite_totales[1]))
+#print(possibilite_totales[0], len(possibilite_totales[0]))
+
+
+
 
 
 # Je dois maintenant faire une liste avec des ensemble de permutations qui fonctionnent en ligne, puis en colonne, puis croiser les deux
-
 combolist_ligne  = []
+
+
+# EN LIGNE
 
 # Je dois essayer de savoir par ligne quels sont les éléments tronqués
 # Les trois premières lignes sont constituées des trois premières matrices, les trois autres lignes les trois autres matrices, etc...
 
+# Je peux essayer de parcourir chaque ligne totale de ma grille et déterminer les chiffres possibles
+
+compteur = 0
+matrices_gauche = []
+matrices_milieu = []
+matrices_droite = []
+for region in matrice_a_trou:
+
+    if compteur % 3 == 0:
+        matrices_gauche.append(region)
+    elif compteur % 3 == 1:
+        matrices_milieu.append(region)
+    else:
+        matrices_droite.append(region)
+    compteur += 1
+
+
+# Maintenant que j'ai séparé par emplacement les matrice, je peux regarder par colonne
+colonnes = []
+colonnes_final = []
+
+
+for i in range (0, 3):
+    colonnes.append(matrices_gauche[0][i])
+    colonnes.append(matrices_gauche[0][i+3])
+    colonnes.append(matrices_gauche[0][i+6])
+
+    colonnes.append(matrices_gauche[1][i])
+    colonnes.append(matrices_gauche[1][i+3])
+    colonnes.append(matrices_gauche[1][i+6])
+
+    colonnes.append(matrices_gauche[2][i])
+    colonnes.append(matrices_gauche[2][i+3])
+    colonnes.append(matrices_gauche[2][i+6])
+
+    colonnes_final.append(colonnes)
+    colonnes = []
+
+
+
+for i in range (0, 3):
+    colonnes.append(matrices_milieu[0][i])
+    colonnes.append(matrices_milieu[0][i+3])
+    colonnes.append(matrices_milieu[0][i+6])
+
+    colonnes.append(matrices_milieu[1][i])
+    colonnes.append(matrices_milieu[1][i+3])
+    colonnes.append(matrices_milieu[1][i+6])
+
+    colonnes.append(matrices_milieu[2][i])
+    colonnes.append(matrices_milieu[2][i+3])
+    colonnes.append(matrices_milieu[2][i+6])
+
+    colonnes_final.append(colonnes)
+    colonnes = []
+
+for i in range (0, 3):
+    colonnes.append(matrices_droite[0][i])
+    colonnes.append(matrices_droite[0][i+3])
+    colonnes.append(matrices_droite[0][i+5])
+
+
+    colonnes.append(matrices_droite[1][i])
+    colonnes.append(matrices_droite[1][i+3])
+    colonnes.append(matrices_droite[1][i+6])
+
+    colonnes.append(matrices_droite[2][i])
+    colonnes.append(matrices_droite[2][i+3])
+    colonnes.append(matrices_droite[2][i+6])
+
+    colonnes_final.append(colonnes)
+    colonnes = []
+
+
+print(colonnes_final)
+print(len(colonnes_final))
+
+
+possibilites_colonnes = []
+liste_chiffres = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for colonne in colonnes_final:
+    chiffres_possibles = []
+    for chiffre in liste_chiffres:
+        if chiffre not in colonne:
+            chiffres_possibles.append(chiffre)
+
+    possibilites_colonnes.append(list(itertools.permutations(chiffres_possibles)))
+     
+
+print(possibilites_colonnes)
+
+
+possibilite_totales_colonnes = []
+compteur = 0
+for colonne in colonnes_final: # je détermine pour chaque région les possibilités
+
+    print(possibilites_ligne_region)
+
+# Il faut que je détermine toutes les combinaisons alors possibles pour la région donnée
+# Chaque liste de possibilités en ligne est de dimension 7
+
+    possibilite_totales.append(list(itertools.permutations(possibilites_ligne_region))) 
+    
+    compteur += len(list(itertools.permutations(possibilites_ligne_region)) ) # il y a n! permutations possibles par matrice
+# print(possibilite_totales, len(possibilite_totales), compteur)
+
+#print(possibilite_totales[0], len(possibilite_totales[0]))
+
+
+
+
+'''
 ligne_placement = [] # [[l,a,b]] l = numéro de la ligne (de 1 à 9), a = numéro de la matrice (de 1 à 9); b = position du tronqué (de 1 à 3)
 for ligne in range(0, 9):
     matrice_gauche = ligne//3
     matrice_milieu = ligne%3   
     matrice_droite = ligne//3 + 2
     print(ligne, matrice_gauche, matrice_milieu)
-
+'''
           
