483c4c8ac8da0c98d1647721f37c1adfb458956c
A. SARRA <alex.boulon7@gmail.com>
2024-02-22 13:50:34 +0100

    Add files via upload

--- main/louis/test_ecriture.py
+++ main/louis/test_ecriture.py
@@ -6,5 +6,7 @@ frame.geometry("320x320")
 
 canvas = Canvas(frame, width=320, height=320, bg="SpringGreen2")
 
-canvas.create_text(100, 100, text="Some Text", fill="black", font=("Helvetica 15 bold")).pack()
+canvas.create_text(100, 100, text="Some Text", fill="black", font=("Helvetica 15 bold"))
+
+canvas.pack()
 frame.mainloop()
